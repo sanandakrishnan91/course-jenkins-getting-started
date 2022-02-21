0b5375f9bff9ac780decb197e46ca620992413c9
sanandakrishnan91 <47587472+sanandakrishnan91@users.noreply.github.com>
2022-02-20 20:09:50 -0800

    Update pipeline.groovy

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,6 +1,8 @@
 pipeline {
     agent any
-    triggers { pollSCM('* * * * *') }
+    triggers {
+        cron('25 * * * *')
+    }
     stages {
         stage('Checkout') {
             steps {
@@ -19,12 +21,12 @@ pipeline {
                     archiveArtifacts 'target/*.jar'
                 }
                 changed {
-                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                        body: "Please go to ${BUILD_URL} and verify the build", 
-                        attachLog: true, 
-                        compressLog: true, 
-                        to: "test@jenkins",
-                        recipientProviders: [upstreamDevelopers(), requestor()]
+                    emailext subject: 'Job \'${JOB_NAME}\' (${BUILD_NUMBER}) is waiting for input',
+                    body: 'Please go to ${BUILD_URL} and verify the build', 
+                    attachLog: true, 
+                    compressLog: true, 
+                    to: "test@jenkins",
+                    recipientProviders: [requestor(), upstreamDevelopers()]
                 }
             }
         }
